547a8d55a9f19614a5298eb409937a944487a0c9
jakobfrick <jakob@dubsmash.com>
2015-11-19 23:41:34 +0100

    Changed to font-awesome and updated

--- js/resume.js
+++ js/resume.js
@@ -4,79 +4,101 @@ function setupResume () {
 		prototype: Object.create(HTMLButtonElement.prototype),
 		extends: 'div'
 	});
-		
+
 	var EmailProto = Object.create(HTMLElement.prototype);
 	EmailProto.createdCallback = function () {
 		// Test if visable
 		if(! (this.offsetParent === null))
-			this.innerHTML = "<span class='octicon octicon-mail'></span>\t" + this.innerHTML;
+			this.innerHTML = "<i class=\"fa fa-envelope-o\"></i>\t" + this.innerHTML;
 	}
-	
+
 	var PhoneProto = Object.create(HTMLElement.prototype);
 	PhoneProto.createdCallback = function () {
 		// Test if visable
 		if(! (this.offsetParent === null))
-			this.innerHTML = "<span class='octicon octicon-device-mobile'></span>\t" + this.innerHTML;
+			this.innerHTML = "<i class=\"fa fa-mobile\"></i>\t" + this.innerHTML;
 	}
-	
+
 	var AddressProto = Object.create(HTMLElement.prototype);
 	AddressProto.createdCallback = function () {
 		// Test if visable
 		if(! (this.offsetParent === null))
-			this.innerHTML = "<span class='octicon octicon-home'></span>\t" + this.innerHTML;
+			this.innerHTML = "<i class=\"fa fa-home\"></i>\t" + this.innerHTML;
 	}
-	
+
 	var GithubProto = Object.create(HTMLElement.prototype);
 	GithubProto.createdCallback = function () {
 		// Test if visable
 		if(! (this.offsetParent === null))
-			this.innerHTML = "<span class='octicon octicon-mark-github'></span>\t" + this.innerHTML;
+			this.innerHTML = "<i class=\"fa fa-github\"></i>\t" + this.innerHTML;
 	}
-	
+
+	var TwitterProto = Object.create(HTMLElement.prototype);
+	TwitterProto.createdCallback = function () {
+		// Test if visable
+		if(! (this.offsetParent === null))
+			this.innerHTML = "<i class=\"fa fa-twitter\"></i>\t" + this.innerHTML;
+	}
+
+	var LinkedInProto = Object.create(HTMLElement.prototype);
+	LinkedInProto.createdCallback = function () {
+		// Test if visable
+		if(! (this.offsetParent === null))
+			this.innerHTML = "<i class=\"fa fa-linkedin-square\"></i>\t" + this.innerHTML;
+	}
+
 	var EmailElement = document.registerElement('r-email', {
 		prototype: EmailProto,
 	});
-	
+
 	var PhoneElement = document.registerElement('r-phone', {
 		prototype: PhoneProto,
 	});
-	
+
 	var AddressElement = document.registerElement('r-address', {
 		prototype: AddressProto,
 	});
-	
+
 	var GithubElement = document.registerElement('r-github', {
 		prototype: GithubProto,
 	});
-	
+
+	var TwitterElement = document.registerElement('r-twitter', {
+		prototype: TwitterProto,
+	});
+
+	var LinkedInElement = document.registerElement('r-linkedin', {
+		prototype: LinkedInProto,
+	});
+
 	var ContactFieldProto = Object.create(HTMLElement.prototype);
 	ContactFieldProto.createdCallback = function () {
 		this.innerHTML = document.getElementsByTagName('r-contact-data')[0].innerHTML;
 	}
-	
+
 	var ContactFieldElement = document.registerElement('r-contact-field', {
 		prototype: ContactFieldProto,
 	});
-	
+
 	var NameFieldProto = Object.create(HTMLElement.prototype);
 	NameFieldProto.createdCallback = function () {
 		this.innerHTML = document.getElementsByTagName('r-author')[0].outerHTML  + "<hr/>" + document.getElementsByTagName('r-subtitle')[0].outerHTML ;
 	}
-	
+
 	var NameFieldElement = document.registerElement('r-name-field', {
 		prototype: NameFieldProto,
 	});
-	
+
 	var PersonalDataProto = Object.create(HTMLElement.prototype);
 	PersonalDataProto.createdCallback = function () {
 		this.insertBefore(document.createElement("r-section-heading"), this.firstChild);
 		this.firstChild.innerHTML = "Personal Data"
 	}
-	
+
 	var PersonalDataElement = document.registerElement('r-personal-data', {
 		prototype: PersonalDataProto
 	});
-	
+
 	var EntryProto = Object.create(HTMLElement.prototype);
 	EntryProto.createdCallback = function () {
 		var caption = this.childNodes[0].nodeValue;
@@ -85,7 +107,7 @@ function setupResume () {
 
 		var without_caption = this.innerHTML.substring(caption.length);
 		this.innerHTML = "<r-entry-caption>" + caption + "</r-entry-caption>" + without_caption;
-	}	
+	}
 
 	var EntryElement = document.registerElement('r-entry', {
 		prototype: EntryProto,
@@ -97,11 +119,11 @@ function setupResume () {
 
 		if(delimiter_pos != -1)
 			this.innerHTML = "<strong>" + this.innerHTML.substring(0, delimiter_pos) + "</strong><i>" + this.innerHTML.slice(delimiter_pos) + "</i>";
-		else	
+		else
 			this.innerHTML = "<strong>" + this.innerHTML + "</strong>";
 		this.parentNode.insertBefore(document.createElement("br"), this.nextSibling);
 	}
-	
+
 	var TitleElement = document.registerElement('r-title', {
 		prototype: TitleProto,
 	});
